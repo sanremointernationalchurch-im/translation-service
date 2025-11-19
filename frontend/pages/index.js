09edd37f7c0b6375ac5cbc0b930213bcd3958867
sanremointernationalchurch-im <sanremointernationalchurch@gmail.com>
2025-11-19 16:01:46 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,2 @@
 import Link from 'next/link'
-import '../styles/global.css'
 export default function Home(){ return (<div className="container"><h1>Translation Service 02</h1><p><Link href="/admin">Admin Dashboard</Link></p><p><Link href="/room/main">Open Room (demo)</Link></p></div>) }
